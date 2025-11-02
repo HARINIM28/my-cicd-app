49b6712a40a8e514a4668d25764537fbdcd08580
HARINI M <mharini2804@gmail.com>
2025-11-02 17:38:06 +0530

    FIX: Add sudo to docker commands and IAM Role

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Exit immediately if any command fails
+set -e
 
 # --- YOU MUST EDIT THESE VARIABLES ---
 AWS_ACCOUNT_ID="711379610491"
@@ -8,25 +10,22 @@ ECR_REPO_NAME="my-cicd-app"
 
 CONTAINER_NAME="my-app"
 
-# Log in to AWS ECR
-# Note: We must use the EC2 instance's permissions, but since we installed awscli...
-# ...and the Jenkinsfile uses aws-creds for the login in the 'Build' stage...
-# ...we need to log in *on the EC2 instance*.
-aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
+# Log in to AWS ECR (this will now work thanks to the IAM Role)
+aws ecr get-login-password --region ${AWS_REGION} | sudo docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
 
-# Stop and remove the old container if it exists
-if [ $(docker ps -a -q -f name=${CONTAINER_NAME}) ]; then
+# Stop and remove the old container if it exists (using sudo)
+if [ $(sudo docker ps -a -q -f name=${CONTAINER_NAME}) ]; then
     echo "Stopping and removing old container..."
-    docker stop ${CONTAINER_NAME}
-    docker rm ${CONTAINER_NAME}
+    sudo docker stop ${CONTAINTAINER_NAME}
+    sudo docker rm ${CONTAINER_NAME}
 fi
 
-# Pull the 'latest' image from ECR
+# Pull the 'latest' image from ECR (using sudo)
 echo "Pulling latest image..."
-docker pull ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO_NAME}:latest
+sudo docker pull ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO_NAME}:latest
 
-# Run the new container
+# Run the new container (using sudo)
 echo "Running new container..."
-docker run -d --name ${CONTAINER_NAME} -p 80:3000 ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO_NAME}:latest
+sudo docker run -d --name ${CONTAINER_NAME} -p 80:3000 ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO_NAME}:latest
 
 echo "Deployment complete!"
